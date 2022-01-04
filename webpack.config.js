2f306fa7eed4fd45cb5fe62c0f026d847a28ceaa
YWTechIT <abcd8637@naver.com>
2022-01-05 07:17:35 +0900

    Add webpack setting

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]?[hash]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "main.css",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./dist"),
+      publicPath: "/assets",
+    },
+    compress: true, // 모든 항목에 대해 GZIP 압축으로 사이트를 최적화시킨다.
+    port: 8080,
+  },
+};
